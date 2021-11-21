6cd51d9508ab37c61ac0deea2e68b06e11752641
fahrizal-fhrz28 <fahrizalomoi40@gmail.com>
2021-11-21 21:34:26 +0700

    Pengaturan layout,menambahkan drawable icon,menambahkan HasilActiVitiy,Menggunakan Intent untuk mengirim extra,Menggunakan Intent untuk mengambil extra,membuat splash,logo,hide actionbar

---
+++ app/src/main/java/com/fhrz/fhrzpraktikumandrop1/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.azhx.azhxpraktikumandrop1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val simpanButton = findViewById<Button>(R.id.simpanButton)
+        val batalButton = findViewById<Button>(R.id.batalButton)
+
+        val namaEditText = findViewById<EditText>(R.id.namaEditText)
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val phoneEditText = findViewById<EditText>(R.id.phoneEditText)
+        val tanggalLahirEditText = findViewById<EditText>(R.id.tanggalLahirEditText)
+        val jumlahSksEditText = findViewById<EditText>(R.id.jumlahSksEditText)
+
+        simpanButton.setOnClickListener {
+            val nama = namaEditText.text.toString()
+            val email = emailEditText.text.toString()
+            var jumlahSks = jumlahSksEditText.text.toString().toInt()
+            jumlahSks = 160 - jumlahSks
+            Toast.makeText(this, "Sisa SKS..., $jumlahSks", Toast.LENGTH_SHORT).show()
+
+            val Intent = Intent(this,ResultActivity::class.java)
+            intent.putExtra("nama",nama)
+            intent.putExtra("email",email)
+            intent.putExtra("jumlah_sks",jumlahSks)
+            startActivity(intent)
+            )
+        }
+
+        batalButton.setOnClickListener {
+            Toast.makeText( this, "batal...", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
